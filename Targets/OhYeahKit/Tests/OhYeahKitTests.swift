77316ba184c23ff76ec97bdcac124ad54d035c8e
bongzniak <bongzniak@gmail.com>
2023-01-19 23:56:01 +0900

    init project

---
+++ Targets/OhYeahKit/Tests/OhYeahKitTests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class OhYeahKitTests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("OhYeahKit", "OhYeahKit")
+    }
+}
---
+++ Targets/OhYeahUI/Tests/OhYeahUITests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class OhYeahUITests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("OhYeahUI", "OhYeahUI")
+    }
+}
