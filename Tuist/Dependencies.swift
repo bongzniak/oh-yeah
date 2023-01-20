749f1766a1a2906ebc063c77662388950215cb0d
bongzniak <bongzniak@gmail.com>
2023-01-20 13:19:57 +0900

    feature: depencenncies 추가

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -8,13 +8,33 @@
 import ProjectDescription
 
 let dependencies = Dependencies(
-    carthage: nil,
+    carthage: CarthageDependencies([
+        .github(path: "CocoaLumberjack/CocoaLumberjack", requirement: .exact("3.8.0"))
+    ]),
     swiftPackageManager: SwiftPackageManagerDependencies(
-        [],
-        baseSettings: .settings(configurations: [
-            .debug(name: "DEV"),
-            .release(name: "PROD")
-        ])
+        [
+            .remote(url: "https://github.com/ReactorKit/ReactorKit.git", requirement: .upToNextMajor(from: "3.2.0")),
+            .remote(url: "https://github.com/ReactiveX/RxSwift.git", requirement: .exact("6.5.0")),
+            .remote(url: "https://github.com/RxSwiftCommunity/RxDataSources.git", requirement: .exact("5.0.2")),
+            .remote(url: "https://github.com/Alamofire/Alamofire", requirement: .upToNextMajor(from: "5.0.0")),
+            .remote(url: "https://github.com/Moya/Moya.git", requirement: .upToNextMajor(from: "15.0.0")),
+            .remote(url: "https://github.com/SnapKit/SnapKit", requirement: .upToNextMajor(from: "5.0.1")),
+            .remote(url: "https://github.com/devxoul/Then", requirement: .upToNextMajor(from: "3.0.0"))
+        ],
+        productTypes: [
+            "ReactorKit": .framework,
+            "RxSwift": .framework,
+            "RxCocoa": .framework,
+            "Moya": .framework,
+            "Alamofire": .framework,
+            "SnapKit": .framework,
+            "Then": .framework,
+            "Kingfisher": .framework
+        ]
+//        baseSettings: .settings(configurations: [
+//            .debug(name: "DEV"),
+//            .release(name: "PROD")
+//        ])
     ),
     platforms: [.iOS]
 )
