4b49fa64afdf6fd1039b31011fa37c654ca2195c
bongzniak <bongzniak@gmail.com>
2023-01-25 14:13:46 +0900

    add: Utility Project 추가

---
+++ Projects/Core/Utility/Project.swift
@@ -0,0 +1,20 @@
+//
+//  Project.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by Bongzniak on 2023/01/20.
+//
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+private let projectName = "Utility"
+private let iOSTargetVersion = "16.0"
+
+let project = Project.framework(
+    name: projectName,
+    platform: .iOS,
+    iOSTargetVersion: iOSTargetVersion,
+    dependencies: [
+        
+    ]
+)
