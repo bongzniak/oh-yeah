82ddaa64da5d75ba4b47fd7f66746e57c35521f3
bongzniak <bongzniak@gmail.com>
2023-02-14 13:11:23 +0900

    feature: model event stream 처리를 위한 파일 추가

--- Projects/Core/Sources/Models/Common/ModelStream.swift
+++ Projects/Core/Sources/Models/Common/ModelStream.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+
+import RxSwift
+
+private var streams: [String: Any] = [:]
+
+extension ModelType {
+    public static var event: PublishSubject<Event> {
+        let key = String(describing: self)
+        if let stream = streams[key] as? PublishSubject<Event> {
+            return stream
+        }
+        let stream = PublishSubject<Event>()
+        streams[key] = stream
+        return stream
+    }
+}
--- Projects/Core/Sources/Models/Common/ModelType.swift
+++ Projects/Core/Sources/Models/Common/ModelType.swift
@@ -10,20 +10,6 @@ import Foundation
 
 import Then
 
-protocol ModelType: Codable, Then {
+public protocol ModelType: Codable, Then {
     associatedtype Event
-    
-    static var dateDecodingStrategy: JSONDecoder.DateDecodingStrategy { get }
-}
-
-extension ModelType {
-    static var dateDecodingStrategy: JSONDecoder.DateDecodingStrategy {
-        return .iso8601
-    }
-    
-    static var decoder: JSONDecoder {
-        let decoder = JSONDecoder()
-        decoder.dateDecodingStrategy = self.dateDecodingStrategy
-        return decoder
-    }
 }
